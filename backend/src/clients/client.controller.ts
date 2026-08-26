ae407a87c64c369e5099b87de233a4eb4f8e4351
Ian Mercadal <ianmercadalf02@gmail.com>
2026-08-26 19:27:49 +0200

    feat: :art: Generando DTO para Client
    Implementando código para crear clientes

--- backend/src/clients/client.controller.ts
+++ backend/src/clients/client.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ClientService } from './client.service';
+import { CreateClientDto } from './dto/create-client.dto';
 
 @Controller('clients')
 export class ClientController {
@@ -7,4 +8,7 @@ export class ClientController {
 
   @Get()
   findAll() { return this.service.findAll(); }
+
+  @Post()
+  create(@Body() dto: CreateClientDto) { return this.service.create(dto); }
 }
--- backend/src/clients/client.service.ts
+++ backend/src/clients/client.service.ts
@@ -2,10 +2,12 @@ import { Inject, Injectable } from '@nestjs/common';
 import type { Repository } from '../common/repository.interface';
 import { Client } from '../common/types/entities';
 import { CLIENT_REPOSITORY } from './client.providers';
+import { CreateClientDto } from './dto/create-client.dto';
 
 @Injectable()
 export class ClientService {
   constructor(@Inject(CLIENT_REPOSITORY) private readonly repo: Repository<Client>) {}
 
   findAll() { return this.repo.findAll(); }
+  create(dto: CreateClientDto) { return this.repo.create(dto); }
 }
