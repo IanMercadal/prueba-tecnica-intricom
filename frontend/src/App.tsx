2315bfdb4efad500eedb690175a7273b74c61ec0
Ian Mercadal <ianmercadalf02@gmail.com>
2026-08-28 09:22:44 +0200

    feat: :art: Enrutado en el frontend
    Creando página para cada entidad previamente a implementar el formulario común

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,25 @@
-import { ClientsPage } from './pages/ClientsPage';
+import { BrowserRouter, Navigate, NavLink, Route, Routes } from 'react-router-dom';
 import { HotelsPage } from './pages/HotelsPage';
+import { ClientsPage } from './pages/ClientsPage';
 import { BookingsPage } from './pages/BookingsPage';
+import './App.css';
 
 export default function App() {
   return (
-    <>
-      <HotelsPage />
-      <ClientsPage />
-      <BookingsPage />
-    </>
+    <BrowserRouter>
+      <nav>
+        <NavLink to="/hotels">Hotels</NavLink>
+        <NavLink to="/clients">Clients</NavLink>
+        <NavLink to="/bookings">Bookings</NavLink>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Navigate to="/hotels" replace />} />
+          <Route path="/hotels" element={<HotelsPage />} />
+          <Route path="/clients" element={<ClientsPage />} />
+          <Route path="/bookings" element={<BookingsPage />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
